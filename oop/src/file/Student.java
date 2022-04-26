4710586b17963924bca0d7df02e61f5edb0b4743
pgtc97 <pgtc97@gmail.com>
2022-04-26 20:16:35 +0700

    update file

---
+++ oop/src/file/Student.java
@@ -0,0 +1,57 @@
+package file;
+
+public class Student {
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String gender, int age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toStringCSV() {
+        return this.id + "," +
+                this.name + "," +
+                this.gender + "," +
+                this.age + "\n";
+    }
+}
---
+++ oop/src/file/TestFile.java
@@ -0,0 +1,54 @@
+package file;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFile {
+
+    private static final String PATH = "student.csv";
+    //phân biệt thrown với throws
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(PATH);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        List<Student> students = new ArrayList<>();
+//        students.add(new Student(1, "a", "b", 10));
+//        students.add(new Student(2, "a", "b", 10));
+//        students.add(new Student(3, "a", "b", 10));
+//        students.add(new Student(4, "a", "b", 10));
+//        students.add(new Student(5, "a", "b", 10));
+//        students.add(new Student(6, "a", "b", 10));
+//        students.add(new Student(7, "a", "b", 10));
+//        students.add(new Student(8, "a", "b", 10));
+//        students.add(new Student(9, "a", "b", 10));
+//
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(PATH));
+//
+//        for (Student student : students) {
+//            bufferedWriter.write(student.toStringCSV());
+//        }
+//        System.out.println("Ghi file thành công!");
+//        bufferedWriter.close();
+        List<Student> students2 = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(PATH));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] arr = line.split(",");
+            students2.add(new Student(Integer.parseInt(arr[0]), arr[1], arr[2], Integer.parseInt(arr[3])));
+        }
+        bufferedReader.close();
+        for (Student s :
+                students2) {
+            System.out.println(s.toStringCSV());
+        }
+
+    }
+}
